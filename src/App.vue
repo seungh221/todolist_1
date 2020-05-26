764c3040ece285d62d6104b73d33c2e2de7f7d8b
seunghee <seungh221@gmail.com>
2020-05-26 17:54:59 +0900

    todo list_1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="col-md-6 offest-md-3">
+       <h1 class="text-center mb-4">Todo 어플리케이션</h1>
+       <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
+      
+       <div class="list-group mb-4">
+          <button class="list-group-item text-left" v-for="todo in activeTodoList" v-bind:key='todo' @click="toggleTodoState(todo)">
+           {{ todo.label }}
+          </button>
+       </div>
+
+       <div class="text-right">
+         <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">할 일</button>
+         <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">완료 </button> 
+         <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">전체</button>        
+       </div>  
+      </div>    
+    </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return{
+      userInput: '',
+      todoList: [],
+      currentStates: 'active'
+    };
+  },
+  computed: {
+    activeToDoList() {
+      return this.todolist.filter(todo => this.currentStates ==='all' && todo.state === this.currentStates);
+  
+    }
+  },
+  methods: { 
+    changeCurrentState(state) {
+      this.currentStates = state;
+    },
+    addNewTodo() {
+      this.todoList.push({
+        label: this.userInput,
+        state: 'active'
+      });
+      this.userInput= '';
+
+    },
+    toggleTodoState(todo){
+      todo.state = todo.state === 'active'? 'done' : 'active' ;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
